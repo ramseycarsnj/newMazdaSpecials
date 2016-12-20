f0e70ce09eff97dba4186f422337c4f55d893283
Joe <joethemovie@gmail.com>
2016-12-20 16:32:25 -0500

    added all files

---
+++ js/master.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+  $("#slideshow > div:gt(0)").hide();
+  setInterval(function() {
+    $('#slideshow > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow');
+  },  3000);
+
+  $("#slideshow2 > div:gt(0)").hide();
+  setInterval(function() {
+    $('#slideshow2 > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow2');
+  },  3000);
+
+  $("#slideshow3 > div:gt(0)").hide();
+  setInterval(function() {
+    $('#slideshow3 > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow3');
+  },  3000);
+
+  $("#slideshow4 > div:gt(0)").hide();
+  setInterval(function() {
+    $('#slideshow4 > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow4');
+  },  3000);
+
+});
